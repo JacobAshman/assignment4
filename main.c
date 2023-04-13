485c7d54f88988944c96ad26b9d49a8a3315a8d5
lswanson119 <123724668+lswanson119@users.noreply.github.com>
2023-04-13 21:19:06 +0000

    Added power the structure and changed variable names

--- main.c
+++ main.c
@@ -11,13 +11,13 @@ typedef struct {
     char name[100];
     char coefficient[20];
     char area[20];
+    double mph60[20];
+    double mph70[20];
+    double mph80[20];
 } Car;
 
 
 void main(int argc, char **argv) {
-
-
-
     char *inputFileName = argv[1];
     char *outputFileName = argv[2];  //allows input and output files to be called in ./a.out command
     printf("Processing text file [%s] . . . \n", inputFileName);
@@ -25,16 +25,12 @@ void main(int argc, char **argv) {
     FILE *f = fopen(inputFileName, "r"); //reads input file
     FILE *out = fopen(outputFileName, "w"); //writes in output file
 
-
-
-
-
-    int *dragCar = (int *) malloc(10000 * sizeof(int));
+    int *areaCars = (int *) malloc(10000 * sizeof(int));
 
     char buffer[100];
     char *line = fgets(buffer, 100, f);
     int totLines = 0;
-    int goodLines = 0;
+    int areaLines = 0;
     int i = 0;
     const int m = 1000;
     Car c[m];
@@ -44,21 +40,24 @@ void main(int argc, char **argv) {
             buffer[n-1] = '\0';
         }
 
-
         char *name = strtok(line, ",");
         char *coefficientToken = strtok(NULL, ",");
         char *areaToken = strtok(NULL, ",");
         double coefficient = atof(coefficientToken);
         double area = atof(areaToken);
+
         if(area != -1) {
             strcpy(c[i].name, name);
             strcpy(c[i].coefficient, coefficientToken);
             strcpy(c[i].area, areaToken);
-            fprintf(out, "%s,%s,%s,%.02f,%.02f,%.02f\n", c[i].name, c[i].coefficient, c[i].area,
-            power(26.8224, coefficient, area), power(31.2928, coefficient, area), power(35.7632, coefficient, area));
-            dragCar[goodLines] = i;
-            goodLines = goodLines + 1;
+            c[i].mph60[i] = power(26.8224, coefficient, area);
+            c[i].mph70[i] = power(31.2928, coefficient, area);
+            c[i].mph80[i] = power(35.7632, coefficient, area);
+            fprintf(out, "%s,%s,%s,%.02f,%.02f,%.02f\n", c[i].name, c[i].coefficient, c[i].area, c[i].mph60[i], c[i].mph70[i], c[i].mph80[i]);
+            areaCars[areaLines] = i;
+            areaLines = areaLines + 1;
             }
+
         line = fgets(buffer, 100, f);
         totLines = 1 + totLines;
         i = i + 1;
@@ -66,22 +65,19 @@ void main(int argc, char **argv) {
 
     printf("--- completed reading %d lines of data\n", totLines);
     printf("Computing the power requirements at 60 mph, 70 mph, and 80 mph ...\nWriting out all automobiles that have a positive drag area to output file %s ... \n", outputFileName);
-    printf("--- completed writing %d lines\n", goodLines);
+    printf("--- completed writing %d lines\n", areaLines);
     printf("Computing pair-based comparisons w.r.t. to drag coefficients and power @ 60 mph ...\n");
-    for(i=0; i<goodLines; i++){
-        double area0 = atof(c[dragCar[i]].area);
-        double power0 = power(26.8224, atof(c[dragCar[i]].coefficient) ,atof(c[dragCar[i]].area));
-        for(int j=0; j<goodLines; j++){
-            double area1 = atof(c[dragCar[j]].area);
-            double power1 = power(26.8224, atof(c[dragCar[j]].coefficient) ,atof(c[dragCar[j]].area));
-            if(area0 > area1 && power0 < power1){
-                printf("--- %s has a larger drag area then %s, but a smaller power required.\n", c[dragCar[i]].name, c[dragCar[j]].name);
-            }
+
+    for(i=0; i<areaLines; i++){
+        double area0 = atof(c[areaCars[i]].area);
+            for(int j=0; j<areaLines; j++){
+                double area1 = atof(c[areaCars[j]].area);
+                    if(area0 > area1 && c[areaCars[i]].mph60[areaCars[i]] < c[areaCars[j]].mph60[areaCars[j]]){
+                        printf("--- %s has a larger drag area then %s, but a smaller power required.\n", c[areaCars[i]].name, c[areaCars[j]].name);
+                        }
+                }   
         }
-    }
 
     fclose(f);
     fclose(out);  //closes both files
-
-
-}
+    }
